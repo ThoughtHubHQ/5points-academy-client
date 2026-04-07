cf2dbb9a5a9634c8c564798d0c451f2da4ca8d22
Ashikur Rahman Bhuiyan <ashikurrb@outlook.com>
2026-04-07 11:39:36 +0600

    updated about us ui

--- app/about-us/_section/AboutUsHero.tsx
+++ app/about-us/_section/AboutUsHero.tsx
@@ -5,7 +5,7 @@ export default function AboutUsHero() {
       <div className="absolute top-1/3 left-1/2 -translate-x-1/2 -translate-y-1/2 w-75 md:w-100 h-75 md:h-100 rounded-full bg-[rgba(13,148,136,0.4)] blur-[90px] md:blur-[120px] -z-10 pointer-events-none"></div>
 
       <h1 className="text-4xl md:text-6xl text-white font-bold text-center mt-8 md:mt-10 tracking-tight">
-        About <br  />
+        About  <br className="block md:hidden" />
         <span className="text-[#2dcdab]">5points Academy</span>
       </h1>
 
